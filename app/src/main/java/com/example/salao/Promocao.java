37527b3fd8cd591ae96aced8069181bd5775f1e0
Caroline Huzalo <carolhuzalo@gmail.com>
2021-05-22 09:20:37 -0300

    fisrt commit

---
+++ app/src/main/java/com/example/salao/Promocao.java
@@ -0,0 +1,4 @@
+package com.example.salao;
+
+public class Promocao {
+}
---
+++ app/src/main/java/com/example/salao/ServicosCabelereiro.java
@@ -0,0 +1,14 @@
+package com.example.salao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ServicosCabelereiro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_servicos_cabelereiro);
+    }
+}
